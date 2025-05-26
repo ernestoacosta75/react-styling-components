41bee7781d7db6f5bd1669de40714d59845f624d
ernestoacosta75 <ernestoacostacuba@yahoo.com.mx>
2025-05-26 17:50:19 +0200

    Installing TailwindCSS v4x

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tailwindcss(),
+    react()
+  ],
   resolve: {
     alias: {
       '@assets': path.resolve(__dirname, './src/assets'),
